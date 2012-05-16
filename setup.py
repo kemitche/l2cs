cb0edb0e1449f9e43905dc67199fd9278746e4e4
Keith Mitchell <kemitche@reddit.com>
2012-05-15 17:04:33 -0700

    Add author info to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ except ImportError:
 setup(
     name='l2cs',
     version="0.1.0",
+    author='Keith Mitchell',
+    author_email='kemitche@reddit.com',
+    description=("Rewrites queries from lucene syntax to"
+                 "Amazon Cloudsearch syntax")
+    license='BSD',
+    url="http://github.com/kemitche/l2cs",
     install_requires=["whoosh"],
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
