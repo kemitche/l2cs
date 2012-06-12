6887bacdc3222725396c99a5371d40b043de0d81
Keith Mitchell <kemitche@reddit.com>
2012-06-12 11:57:14 -0700

    Version bump and fix whoosh dependency

--- l2cs.py
+++ l2cs.py
@@ -16,7 +16,7 @@ import whoosh.qparser.taggers
 import whoosh.query
 
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 
 HANDLERS = {}
--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ except ImportError:
 
 setup(
     name='l2cs',
-    version="0.1.2",
+    version="0.1.3",
     author='Keith Mitchell',
     author_email='kemitche@reddit.com',
     description=("Rewrites queries from lucene syntax to"
                  " Amazon Cloudsearch syntax"),
     license='BSD',
     url="http://github.com/kemitche/l2cs",
-    install_requires=["whoosh"],
+    install_requires=["whoosh>=2.3"],
     py_modules=['l2cs'],
     include_package_data=True,
     test_suite="test_l2cs",
